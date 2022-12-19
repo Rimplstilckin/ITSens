1441e6b9e813cb931104f9d9cdc3ed5a885ffdf7
Rimplstilckin <milos.protic@gmail.com>
2022-12-20 00:48:55 +0100

    Initial chart added

--- js/shapes.js
+++ js/shapes.js
@@ -2,6 +2,11 @@ var dataArray = [5, 11, 18];
 
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width", "100%");
 
-svg.selecAll("rect")
+svg.selectAll("rect")
     .data(dataArray)
-    .enter().append("rect");
+    .enter().append("rect")
+    .attr("height",(d, i)=> d*15)
+    .attr("width","50")
+    .attr("fill", "pink")
+    .attr("x",(d, i) => 60*i)
+    .attr("y",(d, i)=>300-(d*15));
