e4c89e1155966c4463fe9a882873e0a19a304d40
Rimplstilckin <milos.protic@gmail.com>
2022-12-22 22:58:31 +0100

    Graphic beggin

--- js/area.js
+++ js/area.js
@@ -1,15 +1,40 @@
 var dataArray = [25, 26, 28, 32, 37, 45, 55, 70, 90, 120, 135, 150, 160, 168, 172, 177, 189];
 var dataYears = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
 
+var parseDate = d3.timeParse('%Y');
+
 var height = 200;
 var width = 500;
 
-var area=d3.area()
-            .x((d, i)=>i*20)
+var margin = {left:50, right:50, top:40, bottom:0};
+
+var y = d3.scaleLinear().domain([0, d3.max(dataArray)]).range([height, 0]);
+
+var x = d3.scaleTime()
+            .domain(d3.extent(dataYears, (d)=>parseDate(d)))
+            .range([0, width]);
+
+var yAxis = d3.axisLeft(y).ticks(3).tickPadding(10).tickSize(10);
+var xAxis = d3.axisBottom(x);
+
+var area = d3.area()
+            .x((d, i)=>x(parseDate(dataYears[i])))
             .y0(height)
-            .y1((d)=>height-d); 
+            .y1((d)=>y(d)); 
 
-var svg = d3.select('body').attr('height', '500px').append('svg').attr('height', '100%').attr('width', '100%');
-svg.append('path').attr('d', area(dataArray));
+var svg = d3.select('body')
+            .append('svg')
+            .attr('height', '100%')
+            .attr('width', '100%');
 
+var chartGroup = svg.append('g')
+                            .attr('transform', 'translate('+margin.left+', '+margin.top+')')
 
+chartGroup.append('path').attr('d', area(dataArray));
+chartGroup.append('g')
+            .attr('class', 'axis y')
+            .call(yAxis);
+chartGroup.append('g')
+            .attr('class', 'axis x')
+            .attr('transform', 'translate(0, '+height+')')
+            .call(xAxis);
--- js/shapes.js
+++ js/shapes.js
@@ -1,20 +1,39 @@
 var dataArray = [5, 11, 18];
+var dataDays = ['Mon', 'Wed', 'Fri'];
+
+var rainbow = d3.scaleSequential(d3.interpolateRainbow).domain([0, 10]);
+var rainbow2 = d3.scaleSequential(d3.interpolateRainbow).domain([0, 3]);
+
+var x = d3.scaleBand()
+        .domain(dataDays)
+        .range([0,170])
+        .paddingInner(0.1176);
+
+var xAxis = d3.axisBottom(x);
 
 var svg = d3.select("body").append("svg").attr("height","600px").attr("width", "100%");
+
+var cat20 = d3.schemeCategory20;
 
 svg.selectAll("rect")
     .data(dataArray)
     .enter().append("rect")
     .attr("height",(d, i)=> d*15)
     .attr("width","50")
-    .attr("fill", "pink")
+    .attr("fill", (d, i) => rainbow(i))
     .attr("x",(d, i) => 60*i)
     .attr("y",(d, i)=>300-(d*15));
+
+svg.append('g')
+            .attr('class', 'axis x hidden')
+            .attr('transform', 'translate(0, 300)')
+            .call(xAxis);
 
 var newX = 300;
 svg.selectAll("circle.first")
     .data(dataArray)
     .enter().append("circle")
+    .attr('fill', (d, i)=>rainbow2(i))
     .attr("class", "first")
     .attr("cx",(d, i)=>{newX+=d*3+i*20; return newX})
     .attr("cy","100")
@@ -24,6 +43,7 @@ var newX = 600;
 svg.selectAll("ellipse.second")
     .data(dataArray)
     .enter().append("ellipse")
+    .attr('fill', (d, i)=> cat20[i])
     .attr("class", "second")
     .attr("cx",(d, i)=>{newX+=d*3+i*20; return newX})
     .attr("cy","100")
